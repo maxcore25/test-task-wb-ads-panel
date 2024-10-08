963bda11b83ad5ff83a9208bcae0d1df32f8779d
Max <mkorobov2002@mail.ru>
2024-10-08 16:33:47 +0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { AdvertisementModule } from './advertisement/advertisement.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    // MongooseModule.forRoot('mongodb://localhost/nest'),
     AdvertisementModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
