23478ac14086658e3d53fbf55d1ca7ef34b58d0d
Max <mkorobov2002@mail.ru>
2024-10-08 16:56:36 +0300

    Add Swagger properties for schema

--- backend/src/advertisement/dto/create-advertisement.dto.ts
+++ backend/src/advertisement/dto/create-advertisement.dto.ts
@@ -1,8 +1,11 @@
 import { IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAdvertisementDto {
   @IsNumber()
+  @ApiProperty()
   advert: number;
 
+  @ApiProperty()
   date: string;
 }
