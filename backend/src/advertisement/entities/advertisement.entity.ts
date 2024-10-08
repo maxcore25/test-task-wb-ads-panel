f8761890a9e5cd9ffa1a2ab4e5981693a9c64ee7
Max <mkorobov2002@mail.ru>
2024-10-08 16:23:25 +0300

    Update advertisement.entity.ts

--- backend/src/advertisement/entities/advertisement.entity.ts
+++ backend/src/advertisement/entities/advertisement.entity.ts
@@ -1,1 +1,18 @@
-export class Advertisement {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type AdvertisementDocument = HydratedDocument<Advertisement>;
+
+@Schema()
+export class Advertisement {
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  breed: string;
+}
+
+export const AdvertisementSchema = SchemaFactory.createForClass(Advertisement);
