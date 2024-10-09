40c78bdc10d9a3346ed2d4fc7c962172564719ba
engory03 <mkorobov2002@mail.ru>
2024-10-09 15:23:29 +0300

    Import AdvertisementSchema

--- backend/src/advertisement/advertisement.module.ts
+++ backend/src/advertisement/advertisement.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AdvertisementService } from './advertisement.service';
 import { AdvertisementController } from './advertisement.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Advertisement,
+  AdvertisementSchema,
+} from './entities/advertisement.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Advertisement.name, schema: AdvertisementSchema },
+    ]),
+  ],
   controllers: [AdvertisementController],
   providers: [AdvertisementService],
 })
