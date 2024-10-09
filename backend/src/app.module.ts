d2c1580ff5ab9f55d1187bb65ecb4dceab553294
engory03 <mkorobov2002@mail.ru>
2024-10-09 15:23:10 +0300

    Update mongodb url

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AdvertisementModule } from './advertisement/advertisement.module';
 
 @Module({
   imports: [
     // MongooseModule.forRoot('mongodb://mongodb:27017/wb_ads_db'),
+    MongooseModule.forRoot(
+      'mongodb://user:123@mongodb:27017/wb_ads_db?authSource=admin',
+    ),
     AdvertisementModule,
   ],
 })
