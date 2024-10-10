efa12adfcc85a9dd3c24f45638b0bdea69da0c60
engory03 <mkorobov2002@mail.ru>
2024-10-10 15:23:12 +0300

    Fix swagger Advertisement schema

--- backend/src/advertisement/entities/advertisement.entity.ts
+++ backend/src/advertisement/entities/advertisement.entity.ts
@@ -1,16 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { HydratedDocument } from 'mongoose';
 
 export type AdvertisementDocument = HydratedDocument<Advertisement>;
 
 export class Metrics {
   @Prop()
+  @ApiProperty()
   clicks: number;
 
   @Prop()
+  @ApiProperty()
   ctr: number;
 
   @Prop()
+  @ApiProperty()
   cpc: number;
 }
 
@@ -20,21 +24,26 @@ export class Summary extends Metrics {}
 @Schema()
 export class List extends Metrics {
   @Prop()
+  @ApiProperty()
   nmId: number;
 }
 
 @Schema()
 export class Advertisement {
   @Prop()
+  @ApiProperty()
   advert: number;
 
   @Prop()
+  @ApiProperty()
   date: string;
 
   @Prop({ type: Summary })
+  @ApiProperty({ type: () => Summary })
   summary: Summary;
 
   @Prop({ type: [List] })
+  @ApiProperty({ type: [List] })
   list: List[];
 }
 
