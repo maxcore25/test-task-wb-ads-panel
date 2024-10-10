d2ee1cd6339d54c4fa5e6981eca7a112a8ec5c64
engory03 <mkorobov2002@mail.ru>
2024-10-10 14:01:37 +0300

    Write logic in getStats method

--- backend/src/advertisement/advertisement.service.ts
+++ backend/src/advertisement/advertisement.service.ts
@@ -14,95 +14,52 @@ export class AdvertisementService {
   ) {}
 
   async getStats(createAdvertisementDto: CreateAdvertisementDto) {
+    const existingAdvertisement = await this.advertisementModel
+      .findOne(createAdvertisementDto)
+      .exec();
+
+    if (existingAdvertisement) {
+      return existingAdvertisement;
+    }
+
     try {
-      // const res = await axios.post(
-      //   'https://app.marketspace.ru/testing-api/adv/v2/fullstats',
-      //   [
-      //     {
-      //       id: createAdvertisementDto.advert,
-      //       dates: [createAdvertisementDto.date],
-      //     },
-      //   ],
-      // );
+      const res = await axios.post(
+        'https://app.marketspace.ru/testing-api/adv/v2/fullstats',
+        [
+          {
+            id: createAdvertisementDto.advert,
+            dates: [createAdvertisementDto.date],
+          },
+        ],
+      );
 
-      // const createdAdvertisement = new this.advertisementModel(res.data);
-      // createdAdvertisement.save();
+      const nms = res.data[0].days[0].apps.map((app) => ({
+        nmId: app.nm[0].nmId,
+        clicks: app.clicks,
+        cpc: app.cpc,
+        ctr: app.ctr,
+      }));
 
-      // console.log(res.data);
-      // return res.data;
-
-      const existingAdvertisement = this.advertisementModel.findOne({
-        ...createAdvertisementDto,
+      const newAdvertisement = new this.advertisementModel({
+        advert: createAdvertisementDto.advert,
+        date: createAdvertisementDto.date,
+        summary: {
+          clicks: res.data[0].clicks,
+          cpc: res.data[0].cpc,
+          ctr: res.data[0].ctr,
+        },
+        list: nms,
       });
 
-      console.log(existingAdvertisement);
-
-      if (!existingAdvertisement) {
-        const res = await axios.post(
-          'https://app.marketspace.ru/testing-api/adv/v2/fullstats',
-          [
-            {
-              id: createAdvertisementDto.advert,
-              dates: [createAdvertisementDto.date],
-            },
-          ],
-        );
-
-        const nms = res.data[0].days[0].apps.map((app) => ({
-          nmId: app.nm[0].nmId,
-          clicks: app.clicks,
-          cpc: app.cpc,
-          ctr: app.ctr,
-        }));
-
-        const createdAdvertisement = new this.advertisementModel({
-          summary: {
-            clicks: res.data[0].clicks,
-            cpc: res.data[0].cpc,
-            ctr: res.data[0].ctr,
-          },
-          list: nms,
-        });
-
-        return createdAdvertisement.save();
-      }
-
-      // const nms = testAdvFullstats[0].days[0].apps.map((app) => ({
-      //   nmId: app.nm[0].nmId,
-      //   clicks: app.clicks,
-      //   cpc: app.cpc,
-      //   ctr: app.ctr,
-      // }));
-
-      // const createdAdvertisement = new this.advertisementModel({
-      //   summary: {
-      //     clicks: testAdvFullstats[0].clicks,
-      //     cpc: testAdvFullstats[0].cpc,
-      //     ctr: testAdvFullstats[0].ctr,
-      //   },
-      //   list: nms,
-      // });
-      // return createdAdvertisement.save();
-
-      return existingAdvertisement;
+      return await newAdvertisement.save();
     } catch (error) {
       console.log('Error:', error);
       return { error };
     }
   }
 
-  // create(createAdvertisementDto: CreateAdvertisementDto) {
-  //   return 'This action adds a new advertisement';
-  // }
-
   async findAll() {
-    try {
-      const advertisements = await this.advertisementModel.find({});
-      console.log(advertisements);
-      return advertisements;
-    } catch (error) {
-      console.error('Error fetching advertisements:', error);
-    }
+    return await this.advertisementModel.find().exec();
   }
 
   // findOne(id: number) {
