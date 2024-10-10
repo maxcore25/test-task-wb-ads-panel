2a390a272d8bb3e0b83849cbe2ce96909a5236c8
engory03 <mkorobov2002@mail.ru>
2024-10-10 15:05:44 +0300

    Add Swagger description to controllers

--- backend/src/advertisement/advertisement.controller.ts
+++ backend/src/advertisement/advertisement.controller.ts
@@ -6,11 +6,13 @@ import {
   Param,
   Patch,
   Delete,
+  HttpStatus,
 } from '@nestjs/common';
 import { AdvertisementService } from './advertisement.service';
 import { CreateAdvertisementDto } from './dto/create-advertisement.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UpdateAdvertisementDto } from './dto/update-advertisement.dto';
+import { Advertisement } from './entities/advertisement.entity';
 
 @ApiTags('Ads')
 @Controller('ads')
@@ -18,6 +20,12 @@ export class AdvertisementController {
   constructor(private readonly advertisementService: AdvertisementService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Получить рекламную кампанию по номеру РК и дате' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: Advertisement,
+  })
   getStats(@Body() createAdvertisementDto: CreateAdvertisementDto) {
     return this.advertisementService.getStats(createAdvertisementDto);
   }
@@ -28,16 +36,34 @@ export class AdvertisementController {
   // }
 
   @Get()
+  @ApiOperation({ summary: 'Получить все рекламные кампании' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: [Advertisement],
+  })
   findAll() {
     return this.advertisementService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Получить рекламную кампанию по id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: Advertisement,
+  })
   findOne(@Param('id') id: string) {
     return this.advertisementService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Обновить рекламную кампанию по id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: Advertisement,
+  })
   update(
     @Param('id') id: string,
     @Body() updateAdvertisementDto: UpdateAdvertisementDto,
@@ -46,6 +72,12 @@ export class AdvertisementController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Удалить рекламную кампанию по id' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Success',
+    type: Advertisement,
+  })
   remove(@Param('id') id: string) {
     return this.advertisementService.remove(id);
   }
