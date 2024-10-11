f0a44445c1b8bf71b8b4a8fec7026e2f9710209e
engory03 <mkorobov2002@mail.ru>
2024-10-11 12:41:41 +0300

    Create UpperHeader

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -1,25 +1,11 @@
 import { useState } from 'react';
-import { Bell, CircleUser, Home, Menu, Package2 } from 'lucide-react';
+import { Bell, Home, Package2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet';
 import { NoDataAlert } from '../NoDataAlert';
 import { AdStatsForm } from '../AdStatsForm';
 import { AdStatsTable } from '../AdStatsTable';
 import { AdStats } from '@/types';
+import { UpperHeader } from '../UpperHeader';
 
 export function Dashboard() {
   const [adStats, setAdStats] = useState<AdStats | null>(null);
@@ -52,57 +38,7 @@ export function Dashboard() {
         </div>
       </div>
       <div className='flex flex-col'>
-        <header className='flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6'>
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button
-                variant='outline'
-                size='icon'
-                className='shrink-0 md:hidden'
-              >
-                <Menu className='h-5 w-5' />
-                <span className='sr-only'>Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side='left' className='flex flex-col'>
-              <SheetTitle className='sr-only'>Menu</SheetTitle>
-              <SheetDescription className='sr-only'></SheetDescription>
-              <nav className='grid gap-2 text-lg font-medium'>
-                <a
-                  href='#'
-                  className='flex items-center gap-2 text-lg font-semibold'
-                >
-                  <Package2 className='h-6 w-6' />
-                  <span className='sr-only'>WB Ads</span>
-                </a>
-                <a
-                  href='#'
-                  className='mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground'
-                >
-                  <Home className='h-5 w-5' />
-                  Dashboard
-                </a>
-              </nav>
-            </SheetContent>
-          </Sheet>
-          <div className='w-full flex-1'></div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant='secondary' size='icon' className='rounded-full'>
-                <CircleUser className='h-5 w-5' />
-                <span className='sr-only'>Toggle user menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end'>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </header>
+        <UpperHeader />
         <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
           <div className='flex items-center'>
             <h1 className='text-lg font-semibold md:text-2xl'>
---
+++ frontend/src/components/UpperHeader/index.tsx
@@ -0,0 +1,69 @@
+import { CircleUser, Home, Menu, Package2 } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+
+export const UpperHeader = () => {
+  return (
+    <header className='flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6'>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant='outline' size='icon' className='shrink-0 md:hidden'>
+            <Menu className='h-5 w-5' />
+            <span className='sr-only'>Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side='left' className='flex flex-col'>
+          <SheetTitle className='sr-only'>Menu</SheetTitle>
+          <SheetDescription className='sr-only'></SheetDescription>
+          <nav className='grid gap-2 text-lg font-medium'>
+            <a
+              href='#'
+              className='flex items-center gap-2 text-lg font-semibold'
+            >
+              <Package2 className='h-6 w-6' />
+              <span className='sr-only'>WB Ads</span>
+            </a>
+            <a
+              href='#'
+              className='mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground'
+            >
+              <Home className='h-5 w-5' />
+              Dashboard
+            </a>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className='w-full flex-1'></div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='secondary' size='icon' className='rounded-full'>
+            <CircleUser className='h-5 w-5' />
+            <span className='sr-only'>Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+  );
+};
