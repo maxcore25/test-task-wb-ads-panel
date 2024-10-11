2e8da717f7d7a46375c9b9e27516ae77683f8e3f
engory03 <mkorobov2002@mail.ru>
2024-10-11 10:48:14 +0300

    Add gap between form and table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,8 +9,10 @@ function App() {
   return (
     <div className='container mx-auto p-4'>
       <h1 className='mb-4 text-2xl font-bold'>Advertisement Statistics</h1>
-      <AdStatsForm onSubmit={setAdStats} />
-      {adStats && <AdStatsTable data={adStats} />}
+      <div className='grid gap-8'>
+        <AdStatsForm onSubmit={setAdStats} />
+        {adStats && <AdStatsTable data={adStats} />}
+      </div>
     </div>
   );
 }
