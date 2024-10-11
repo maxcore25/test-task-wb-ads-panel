e20e60814ed57d8960bf8ffcaef78a1aa28d55a4
engory03 <mkorobov2002@mail.ru>
2024-10-11 15:09:25 +0300

    Create testAd

---
+++ frontend/src/temp/index.ts
@@ -0,0 +1,42 @@
+export const testAd = {
+  _id: '6706b10cbd3334e5fbd48e77',
+  advert: 19447497,
+  date: '2024-10-08',
+  summary: {
+    clicks: 667,
+    ctr: 3.55,
+    cpc: 3.75,
+    _id: '6706b10cbd3334e5fbd48e78',
+  },
+  list: [
+    {
+      nmId: 186652711,
+      clicks: 0,
+      ctr: 0,
+      cpc: 0,
+      _id: '6706b10cbd3334e5fbd48e79',
+    },
+    {
+      nmId: 186652711,
+      clicks: 21,
+      ctr: 1.75,
+      cpc: 8.17,
+      _id: '6706b10cbd3334e5fbd48e7a',
+    },
+    {
+      nmId: 186652711,
+      clicks: 249,
+      ctr: 3.58,
+      cpc: 3.69,
+      _id: '6706b10cbd3334e5fbd48e7b',
+    },
+    {
+      nmId: 186652711,
+      clicks: 397,
+      ctr: 3.73,
+      cpc: 0,
+      _id: '6706b10cbd3334e5fbd48e7c',
+    },
+  ],
+  __v: 0,
+};
