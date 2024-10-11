6fb410d6806be7126942b372aba384a6ea065223
engory03 <mkorobov2002@mail.ru>
2024-10-11 11:56:10 +0300

    Import Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from 'react';
-import { AdStatsForm } from './components/AdStatsForm';
-import { AdStatsTable } from './components/AdStatsTable';
-import { AdStats } from './types';
+import { Dashboard } from './components/Dashboard';
 
 function App() {
-  const [adStats, setAdStats] = useState<AdStats | null>(null);
-
   return (
-    <div className='container mx-auto p-4'>
-      <h1 className='mb-4 text-2xl font-bold'>Advertisement Statistics</h1>
-      <div className='grid gap-8'>
-        <AdStatsForm onSubmit={setAdStats} />
-        {adStats && <AdStatsTable data={adStats} />}
-      </div>
-    </div>
+    // <div className='container mx-auto p-4'>
+    //   <h1 className='mb-4 text-2xl font-bold'>Advertisement Statistics</h1>
+    //   <div className='grid gap-8'>
+    //     <AdStatsForm onSubmit={setAdStats} />
+    //     {adStats && <AdStatsTable data={adStats} />}
+    //   </div>
+    // </div>
+    <Dashboard />
   );
 }
 
