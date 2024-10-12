998a69ad6b941c768ee16800fc6a0a497de904cb
engory03 <mkorobov2002@mail.ru>
2024-10-12 03:39:24 +0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,17 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdvertisementModule } from './advertisement/advertisement.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['../.env'],
+      isGlobal: true,
+    }),
     // MongooseModule.forRoot('mongodb://mongodb:27017/wb_ads_db'),
     MongooseModule.forRoot(
-      'mongodb://user:123@mongo:27017/wb_ads_db?authSource=admin',
+      `mongodb://${process.env.MONGO_USER || 'user'}:${process.env.MONGO_PASSWORD || '123'}@mongo:27017/wb_ads_db?authSource=admin`,
     ),
     AdvertisementModule,
   ],
